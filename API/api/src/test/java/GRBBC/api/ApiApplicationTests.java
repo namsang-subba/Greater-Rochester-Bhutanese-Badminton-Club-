019fd73133e7fec8d2394869dbbb4b67a9adf74a
Namsang <namsang.subba26@gmail.com>
2024-07-21 16:42:17 -0400

    spring layout downloaded

---
+++ API/api/src/test/java/GRBBC/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package GRBBC.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
